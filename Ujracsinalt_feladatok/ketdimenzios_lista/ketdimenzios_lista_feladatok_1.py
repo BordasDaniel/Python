4a4033fca1fc91f61e14f1c36fec233c347a60e9
Bordás Dániel <79927720+BordasDaniel@users.noreply.github.com>
2023-02-13 18:53:31 +0100

    Add files via upload

---
+++ Ujracsinalt_feladatok/ketdimenzios_lista/ketdimenzios_lista_feladatok_1.py
@@ -0,0 +1,15 @@
+#1. Feladat
+"""
+Írj egy programot, ami 4 darab három elemű [0;25] intervallumban lévő véletlen egész számokat tartalmazó listát generál, és ezeket a listákat egy 'tarolo' nevű listába helyezi. A program írja ki a képernyőre a 'tarolo' nevű lista tartalmát!
+"""
+
+import random
+
+def main():
+    tarolo = []
+    for x in range(4):
+        lista = [random.randint(0, 25) for x in range(3)]
+        tarolo.append(lista)
+    print(tarolo)
+
+main()
